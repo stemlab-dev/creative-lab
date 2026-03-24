6b39389121b389cdf415889de471008a7738c094
stemlab-dev <steminnovatorslab@gmail.com>
2026-03-24 11:55:11 +0100

    upload files
    done with index page, will continue responsive later

---
+++ forget.js
@@ -0,0 +1,41 @@
+// const name = 'james';
+// let age = 30;
+// let city = 'New York';
+
+// const lastName = 'abdul';
+// age = 31;
+// let balance = 1000;
+// let creedit = 500;
+// let totalBalance = balance + creedit;
+// console.log(totalBalance);
+// console.log(name);
+// // print(totalBalance);
+
+// // 60 + 90
+
+// /*
+// comment 1
+// comment 2
+// */
+
+// const student = 'John Doe';
+
+// // const gender = "male";
+// // const isMale = true;
+// const accountA = '123456789';
+// const accountB = '123456789';
+
+// const isTheSame = accountA == accountB;
+// let school = 'Bayero University Kano';
+// console.log(school);
+
+// console.log(name);
+// console.log(age);
+// console.log(city);
+
+const login = () => {
+	const firstName = document.getElementById('firstName').value;
+	const lastName = document.getElementById('lastName').value;
+	console.log(firstName);
+	console.log(lastName);
+};
